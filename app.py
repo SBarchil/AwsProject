3121229d46eb04f1a42cac160ebde42b60bc9bb2
said barchil <sbarchil@debian.debian.local>
2022-05-19 21:00:59 -0400

    add pages

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ app= Flask(__name__)
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")	
+
+@app.route('/employeelist')
+def employeelist():
+	return render_template("employeelist.html")	
+
+@app.route('/newemployee')
+def newemployee():
+	return render_template("newemployee.html")	
